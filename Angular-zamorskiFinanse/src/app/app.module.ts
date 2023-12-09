6559626aea21a6edb60ad9f281edc9defb5cd471
Soszka <bsocha1998@gmail.com>
2023-12-09 18:16:48 +0100

    project initialized

---
+++ Angular-zamorskiFinanse/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent
+  ],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
