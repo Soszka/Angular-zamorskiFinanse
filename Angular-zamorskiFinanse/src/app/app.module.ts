25f3d8fa2aaa3f7fe5e10cae8d6588bebea50fc2
Soszka <bsocha1998@gmail.com>
2023-12-12 19:44:25 +0100

    hero & values sections finished

--- Angular-zamorskiFinanse/src/app/app.module.ts
+++ Angular-zamorskiFinanse/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
+import { ValuesComponent } from './values/values.component';
+import { AboutComponent } from './about/about.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeroComponent,
+    ValuesComponent,
+    AboutComponent
   ],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
