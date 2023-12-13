385850dc09b63975c39692f5aa451f5d1943c3da
Soszka <bsocha1998@gmail.com>
2023-12-13 16:57:14 +0100

    title component created

--- Angular-zamorskiFinanse/src/app/app.module.ts
+++ Angular-zamorskiFinanse/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeroComponent } from './hero/hero.component';
 import { ValuesComponent } from './values/values.component';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AboutComponent } from './about/about.component'
     NavigationComponent,
     HeroComponent,
     ValuesComponent,
-    AboutComponent
+    AboutComponent,
+    TitleComponent
   ],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
