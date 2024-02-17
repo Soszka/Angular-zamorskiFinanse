b28993f6d7b97c0640a740be4703db1880146ec3
Soszka <bsocha1998@gmail.com>
2024-02-17 09:17:35 +0100

    company section removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CompanyComponent } from './company/company.component';
 import { OfertsComponent } from './oferts/oferts.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'firma', component: CompanyComponent },
   { path: 'oferty', component: OfertsComponent },
 ];
 
